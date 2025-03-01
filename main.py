8c34eee5cad0e10eb594f034add7ca4308c0c069
Daniel BenShushan <106238883+DevOpsDani@users.noreply.github.com>
2025-03-01 23:24:49 +0200

    Rename main.py to tunnel.py (#1)
    * Rename main.py to tunnel.py
    
    * Create main.py
    
    * Create instance_mapping.yml
    
    * Create tunnel_sessions.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- main.py
+++ main.py
@@ -1,103 +1,46 @@
-import yaml
-import subprocess
-import socket
-import random
 import logging
-import os
+import sys
+from tunnel import TunnelConfig, PortFinder, ExecuteTunnel
 
+logging.basicConfig(level=logging.INFO)
 
-class TunnelConfig:
-    def __init__(self, config_file: str, env: str):
-        self._env = env
-        self._config_file = config_file    
-        self._sessions = self.load_sessions()
+if len(sys.argv) < 3:
+    print("Usage: python main.py <env> <config_file_path>")
+    sys.exit(1)
+
+def main():
+    """
+    Main function to open multipile tunneling sessions
+    Use the following way: python main.py env tunnel_sessions.yml 
+    """
+    env=sys.argv[1]
+    config_file = sys.argv[2]
+    config = TunnelConfig(config_file, env)
+    instance = config.get_instance()
+    port = PortFinder()
+
+    for session in config.test_targets:
+        host = session['host']
+        dest_port = session['destination_port']
+        region = session['region']
+        local_port = port.find_unused_port()
+
         
-    @property
-    def test_targets(self):
-        """
-        Retrieves the relevant hosts & ports for the requested environment
-        """
-        return self._sessions.get(self._env)
+        tunnel = ExecuteTunnel(
+            env, 
+            host, 
+            local_port, 
+            dest_port, 
+            instance, 
+            region
+        )
         
-    def load_sessions(self):
-        """
-        Load tunnel session mappings from env file
-        """
-        try:
-            with open(self._config_file, "r") as config:
-                data = yaml.safe_load(config)
-                return data.get("tunnel_sessions", {})
-        except (FileNotFoundError, yaml.YAMLError) as e:
-            print(f"Error loading tunnel sessions: {e}")
-            return {}
+        result = tunnel.execute_tunnel()
         
+        if result["status"] == "success":
+            logging.info(f"Tunnel established successfully for {host}:{dest_port} on local port {local_port}, with PID {result['pid']}")
+        else:
+            logging.error(f"Failed to establish tunnel: {result['error']}")
     
-    def get_instance(self):
-        """    
-        Return relevant instance per environment 
-        """
-        try: 
-            config_file = "./instance_mapping.yml"
-            with open(config_file, "r") as config:
-                data = yaml.safe_load(config)
-                return data.get("instance_mapping", {}).get(self._env, "Unknown environment")
-        except (FileNotFoundError, yaml.YAMLError) as e:
-            print(f"Error loading tunnel sessions: {e}")
-            return {}
-    
-class PortFinder:
-    def __init__(self, port_range=(32768, 61000), max_attempts=20):
-        self.port_range = port_range
-        self.max_attempts = max_attempts
-
-    def is_port_in_use(self, port):
-        """Check if a port is already in use."""
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            return s.connect_ex(("localhost", port)) == 0
-
-    def find_unused_port(self):
-        """
-        Find an available port within the specified range.
-        Stops after max_attempts to avoid infinite loops.
-        """
-        for _ in range(self.max_attempts):
-            port = random.randint(*self.port_range)
-            if not self.is_port_in_use(port):
-                return port
-
-        logging.error("Failed to find an unused port after multiple attempts.")
-        return None
-
-
-class ExecuteTunnel:
-    def __init__(self, env: str, host: str, local_port: int, dest_port: int, instance: str, region: str, profile: str = "default"):
-        self._env = env
-        self._host = host
-        self._local_port = local_port
-        self._dest_port = dest_port
-        self._instance = instance
-        self._region = region
-        self._profile = profile
-
-    def execute_tunnel(self):
-        """
-        Execute the SSM tunnel command to establish port forwarding.
-        Returns the output of the command or an error message.
-        """
-        command = [
-            "aws", "ssm", "start-session",
-            "--target", self._instance,
-            "--region", self._region,
-            "--profile", self._profile,
-            "--document-name", "AWS-StartPortForwardingSessionToRemoteHost",
-            "--parameters", f'{{"host":["{self._host}"],"portNumber":["{self._dest_port}"], "localPortNumber":["{self._local_port}"]}}'
-        ]
-        
-        try:
-            process = subprocess.Popen(
-                command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid
-            )
-            return {"status": "success", "pid": process.pid}
-        
-        except Exception as e:
-            return {"status": "error", "error": str(e)}
+if __name__ == "__main__":
+    main()
